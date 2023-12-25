b7c494d4c7fde297fdb7e464a3caefe93816f477
yh9035926 <yh9035926@naver.com>
2023-12-25 09:58:14 +0900

    product reducer

---
+++ src/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import post from "./post";
+import user from "./user";
+const rootReducer = combineReducers({
+  post,
+  user,
+});
+
+export default rootReducer;
